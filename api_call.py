b809d273023160fb0ff91679a609e9c8939b979a
MainaKamau92 <lewiikamaa8@gmail.com>
2022-12-30 13:58:10 +0300

    update recieve money service to v3
    Signed-off-by: MainaKamau92 <lewiikamaa8@gmail.com>

--- api_call.py
+++ api_call.py
@@ -1,12 +1,11 @@
 # TODO: Remove once done testing
 import os
+from datetime import datetime
 
-from src.jengaapi.account_services import account_services
 from src.jengaapi.auth import JengaAPI
 from src.jengaapi.exceptions import generate_reference
+from src.jengaapi.receive_money_services import receive_money_service
 from src.jengaapi.send_money_imt_services import send_money_imt_service
-from src.jengaapi.send_money_services import send_money_service
-from datetime import datetime
 
 API_KEY = os.getenv("API_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
@@ -24,43 +23,15 @@ DATE = datetime.now().date().strftime("%Y-%m-%d")
 DEST_ACC_NO = "0250163591202"
 
 payload = {
-    "source": {
-        "countryCode": "KE",
-        "name": "Merchant name",
-        "accountNumber": ACCOUNT_NUMBER
-    },
-    "sender": {
-        "name": "Sender Name",
-        "documentType": "NationalId",
-        "documentNumber": "12345",
-        "countryCode": "KE",
-        "mobileNumber": "0763000000",
-        "email": "sender.name@example.com",
-        "address": "Sender Address"
-    },
-    "destination": {
-        "type": "mobile",
-        "countryCode": "KE",
-        "name": "A N.Other",
-        "walletName": "Mpesa",
-        "accountNumber": DEST_ACC_NO,
-        "mobileNumber": "0763123456",
-        "documentType": "NationalId",
-        "documentNumber": "123456",
-        "address": "Destination Address"
-    },
-    "transfer": {
-        "type": "Pesalink",
-        "amount": TRANSFER_AMOUNT,
-        "currencyCode": CURRENCY_CODE,
-        "date": DATE,
-        "reference": REFERENCE_NO,
-        "description": "some remarks here"
-    }
+    "billerCode": "320320",
+    "customerRefNumber": "111222",
+    "amount": TRANSFER_AMOUNT,
+    "amountCurrency": CURRENCY_CODE,
+    "countryCode": COUNTRY_CODE,
 }
 api = JengaAPI(API_KEY, CONSUMER_SECRET, MERCHANT_CODE, BASE_URL)
 api_token = api.authorization_token
-data = (TRANSFER_AMOUNT, CURRENCY_CODE, REFERENCE_NO, "A N.Other", ACCOUNT_NUMBER)
+data = ("0766000000", ACCOUNT_NUMBER, TRANSFER_AMOUNT, CURRENCY_CODE, REFERENCE_NO)
 signature = api.signature(data)
-res = send_money_imt_service.send_pesa_link_to_mobile_number(signature, api_token, **payload)
+res = receive_money_service.bill_validation(signature, api_token, **payload)
 print(res)
