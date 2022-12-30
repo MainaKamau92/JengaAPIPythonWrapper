b31d77f1ed0b71546044012ec5cf750bbd44b651
MainaKamau92 <lewiikamaa8@gmail.com>
2022-12-30 14:48:41 +0300

    update miscellaneous(airtime purchase, fx and kyc) to v3
    Signed-off-by: MainaKamau92 <lewiikamaa8@gmail.com>

--- api_call.py
+++ api_call.py
@@ -7,6 +7,7 @@ from src.jengaapi.exceptions import generate_reference
 from src.jengaapi.receive_money_queries_services import receive_money_queries_service
 from src.jengaapi.receive_money_services import receive_money_service
 from src.jengaapi.send_money_imt_services import send_money_imt_service
+from src.jengaapi.uncategorized_services import miscellaneous_services
 
 API_KEY = os.getenv("API_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
@@ -15,24 +16,27 @@ ACCOUNT_NAME = os.getenv("ACCOUNT_NAME")
 ACCOUNT_NUMBER = os.getenv("ACCOUNT_NUMBER")
 BASE_URL = os.getenv("JENGA_API_BASE_URL", "https://uat.finserve.africa/")
 SOURCE_ACCOUNT_NUMBER = ACCOUNT_NUMBER
-TRANSFER_AMOUNT = "1000.00"
+TRANSFER_AMOUNT = 1000.00
 CURRENCY_CODE = "KES"
 COUNTRY_CODE = "KE"
 FOREIGN_CURRENCY = "USD"
 REFERENCE_NO = generate_reference()
 DATE = datetime.now().date().strftime("%Y-%m-%d")
 DEST_ACC_NO = "0250163591202"
-
+DOCUMENT_NUMBER = "29009055"
 payload = {
-    "billerCode": "320320",
-    "customerRefNumber": "111222",
-    "amount": TRANSFER_AMOUNT,
-    "amountCurrency": CURRENCY_CODE,
-    "countryCode": COUNTRY_CODE,
+   "identity": {
+      "documentType": "ID",
+      "firstName": "Lewis",
+      "lastName": "Maina",
+      "dateOfBirth": "1992-07-05",
+      "documentNumber": DOCUMENT_NUMBER,
+      "countryCode": COUNTRY_CODE,
+   }
 }
 api = JengaAPI(API_KEY, CONSUMER_SECRET, MERCHANT_CODE, BASE_URL)
 api_token = api.authorization_token
-data = ("0766000000", ACCOUNT_NUMBER, TRANSFER_AMOUNT, CURRENCY_CODE, REFERENCE_NO)
+data = (COUNTRY_CODE, DOCUMENT_NUMBER)
 signature = api.signature(data)
-res = receive_money_queries_service.get_all_billers(api_token, 1, 10, "utilities")
+res = miscellaneous_services.kyc(signature, api_token, **payload)
 print(res)
--- src/jengaapi/uncategorized_services.py
+++ src/jengaapi/uncategorized_services.py
@@ -1,69 +1,29 @@
-import json
-
-import requests
+from typing import Union
 
 from . import BASE_URL
-from .exceptions import generate_reference, handle_response
+from .utils import prepare_request_header, send_post_request
 
 
-class UncategorizedServices:
-    def __init__(self, token):
-        self.token = token
-        self.headers = {
-            'Content-Type': 'application/json',
-            'Authorization': self.token
-        }
-        self.reference_no = generate_reference()
+class MiscellaneousServices:
+    service_url = f'{BASE_URL}v3-apis/'
 
-    def purchase_airtime(self, signature, country_code, mobile_number, airtime_amount, telco):
-        ref = self.reference_no
-        # signature = API.signature((MERCHANT_CODE, telco, airtime_amount, ref))
-        self.headers["signature"] = signature
-        payload = {
-            "customer": {
-                "countryCode": country_code,
-                "mobileNumber": mobile_number
-            },
-            "airtime": {
-                "amount": airtime_amount,
-                "reference": ref,
-                "telco": telco
-            }
-        }
-        url = BASE_URL + f'transaction/v2/airtime'
-        response = requests.post(url, headers=self.headers, data=json.dumps(payload))
-        formatted_response = handle_response(response)
-        return formatted_response
+    @classmethod
+    def purchase_airtime(cls, signature: str, api_token: str, **payload: dict):
+        headers = prepare_request_header(signature, api_token)
+        url = f"{cls.service_url}transaction-api/v3.0/airtime"
+        return send_post_request(headers=headers, payload=payload, url=url)
 
-    def get_forex_rates(self, country_code, currency_code):
-        payload = {
-            "countryCode": country_code,
-            "currencyCode": currency_code
-        }
-        url = BASE_URL + f'transaction/v2/foreignexchangerates'
-        response = requests.post(url, headers=self.headers, data=json.dumps(payload))
-        formatted_response = handle_response(response)
-        return formatted_response
+    @classmethod
+    def get_forex_rates(cls, signature: Union[None, str], api_token: str, **payload: dict):
+        headers = prepare_request_header(signature, api_token)
+        url = f"{cls.service_url}transaction-api/v3.0/foreignExchangeRates"
+        return send_post_request(headers=headers, payload=payload, url=url)
 
-    def id_search_and_verification(self, document_type, signature, **kwargs):
-        first_name = kwargs.get("first_name")
-        last_name = kwargs.get("last_name")
-        date_of_birth = kwargs.get("date_of_birth")
-        document_number = kwargs.get("document_number")
-        country_code = kwargs.get("country_code")
-        # signature = API.signature((MERCHANT_CODE, document_number, self.country_code))
-        self.headers["signature"] = signature
-        payload = {
-            "identity": {
-                "documentType": document_type,
-                "firstName": first_name,
-                "lastName": last_name,
-                "dateOfBirth": date_of_birth,
-                "documentNumber": document_number,
-                "countryCode": country_code
-            }
-        }
-        url = BASE_URL + f'customer/v2/identity/verify'
-        response = requests.post(url, headers=self.headers, data=json.dumps(payload))
-        formatted_response = handle_response(response)
-        return formatted_response
+    @classmethod
+    def kyc(cls, signature: str, api_token: str, **payload: dict):
+        headers = prepare_request_header(signature, api_token)
+        url = f"{cls.service_url}v3.0/validate/identity"
+        return send_post_request(headers=headers, payload=payload, url=url)
+
+
+miscellaneous_services = MiscellaneousServices()
